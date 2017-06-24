01e0ea942ee9c0a784002e84508357277eb05e43
Luiz Fritsch <fritsch.luiz@gmail.com>
2017-06-23 21:16:59 -0300

    Modificações: -Nao lembro

--- Controller/cadastrarUsuario.php
+++ Controller/cadastrarUsuario.php
@@ -3,18 +3,6 @@
 include '../Model/BD/ConexaoBanco.php';
 ini_set('display_errors', 1);
 
-// $mysqli = new mysqli("localhost","bernardino","mysql", "catalog");
-// $sku = $_POST['sku'];
-// $name = $_POST['name'];
-// $price = $_POST['price'];
-// $sql = "INSERT INTO products SET id=NULL, sku=$sku, name=$name, price=$price";
-// if ($mysqli->query($sql) === TRUE) {
-//     echo "New record created successfully";
-// } else {
-//     echo "Error: " . $mysqli . "<br>" . $mysqli->error;
-// }
-
-
 $usuario = $_POST['user'];
 
 if ($usuario == '1') {
@@ -34,8 +22,7 @@ if ($usuario == '1') {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-//echo "$sku > $name  > $price";
-
+    
     $query = "INSERT INTO professor (nome, senha, email)
     VALUES ('$nome','$senha','$email')";
     $conexao->requisicoesBanco($query);
