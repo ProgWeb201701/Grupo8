628eed7bb6a04a71a73c3713c46482a1dd5ba838
DESKTOP-PUJK51S\junin <juninho270199iderli@gmail.com>
2017-06-27 15:30:59 -0300

    correção em views

---
+++ View/inicioAluno.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/css.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <div class="navbar navbar-default" role="navigation">
+                <div class="container">
+                    <div class="navbar-header col-md-11">
+                        <a href="#" class="navbar-brand" title="Página inicial">
+                            Gerenciador de TCC
+                        </a>
+                    </div>
+                    <div class="col-md-1">
+                        <div class="dropdown">
+                            <button class="btn btn-primary" data-toggle="dropdown">Dropdown Example
+                                <span class="caret"></span></button>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Editar dados</a></li>
+                                <li><a href="../index.php">Sair</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>   
+            <div>
+
+            </div>    
+        </div>
+
+    </body>
+</html>
